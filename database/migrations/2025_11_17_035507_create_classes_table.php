2af77b82db3e49b25243a2ace29f89ec8417b480
Fatimah137 <fatimahfauziag@gmail.com>
2025-11-17 11:13:24 +0700

    update migrations and database config for devcourse

---
+++ database/migrations/2025_11_17_035507_create_classes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->integer('price')->default(0);
+            $table->foreignId('instructor_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
